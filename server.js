3194fcce435d422c088b12ab1788953ea841f59d
ramanuj-droid <sage.rama.pb@gmail.com>
2025-09-17 18:44:09 +0530

    Update server.js
    Port changed to 5000 to 3000

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
+
